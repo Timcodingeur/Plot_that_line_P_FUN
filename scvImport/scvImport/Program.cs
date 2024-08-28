37ca6856f6b2b3a3fc6d10bd048180191867c9f5
Timothy Noah Jotterand <pw67ehi@eduvaud.ch>
2024-08-28 14:45:35 +0200

    bob

---
+++ scvImport/scvImport/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace scvImport
+{
+    internal class Program
+    {
+
+        public static List<string[]> readPath(string bob)
+        {
+            List<string[]> result = new List<string[]>();
+            using (StreamReader sr = new StreamReader(bob))
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var val = line.Split(",");
+                    result.Add()
+                }
+        }
+        static void Main(string[] args)
+        {
+            List <>= readPath("");
+        }
+    }
+}
