e7d3f0f4e5540099b72b8e389f6c66b5ba4ba1da
timcodingeur <timi.jotterand@gmail.com>
2024-09-25 13:53:22 +0200

    j'ai fini les test unitaire (j'en ai fait 3) pour tester les plotSignalData et la methode search. et j'y ai mis des commentaire

--- Plot_that_line_P_FUNTests/CalculateTests.cs
+++ Plot_that_line_P_FUNTests/CalculateTests.cs
@@ -12,9 +12,10 @@ namespace Plot_that_line_P_FUN.Tests
     [TestClass()]
     public class CalculateTests
     {
+        /// <summary>
+        /// ici on test que les fichier csv sont bien lu. donc on test la lecture et si il y'a une erreur dans la lecture du fichier il fail
+        /// </summary>
         [TestMethod()]
-
-
         public void ReadCsvTest()
         {
             List<CryptoData> data = new List<CryptoData>();
@@ -37,22 +38,94 @@ namespace Plot_that_line_P_FUN.Tests
             
         }
 
+        /// <summary>
+        /// ici on créé des donnée fictive d'un crypto. on en fait des tableau et on test si il le tableau a bien été généré
+        /// </summary>
         [TestMethod()]
         public void PlotSignalDataTest()
         {
-            Assert.Fail();
+            
+            var calculate = new Calculate();
+
+            // donnée fictive
+            var cryptoData = new List<CryptoData>
+            {
+                new CryptoData { Date = new DateTime(2021, 1, 1), Close = 30000 },
+                new CryptoData { Date = new DateTime(2021, 1, 2), Close = 32000 },
+                new CryptoData { Date = new DateTime(2021, 1, 3), Close = 31000 },
+            };
+
+            
+            calculate.PlotSignalData("Bitcoin", cryptoData);
+
+            
+            double[] expectedXValues = cryptoData.Select(c => c.Date.ToOADate()).ToArray();
+            double[] expectedYValues = cryptoData.Select(c => c.Close).ToArray();
+
+            
+            Assert.IsNotNull(expectedXValues);
+            Assert.IsNotNull(expectedYValues);
+            Assert.AreEqual(3, expectedXValues.Length);
+            Assert.AreEqual(3, expectedYValues.Length);
+            Assert.AreEqual(30000, expectedYValues[0]);
+            Assert.AreEqual(32000, expectedYValues[1]);
         }
 
+        /// <summary>
+        /// ici on créé des donnée fictive d'un crypto, différence avec l'autre methode
+        /// est qu'on test avec des date.
         [TestMethod()]
         public void PlotSignalDataCalTest()
         {
-            Assert.Fail();
+            
+            var calculate = new Calculate();
+
+            // donnée fictive
+            var cryptoData = new List<CryptoData>
+            {
+                new CryptoData { Date = new DateTime(2021, 1, 1), Close = 30000 },
+                new CryptoData { Date = new DateTime(2021, 1, 2), Close = 32000 },
+                new CryptoData { Date = new DateTime(2021, 1, 3), Close = 31000 },
+            };
+
+            DateTime openDate = new DateTime(2021, 1, 2);
+            DateTime endDate = new DateTime(2021, 1, 3);
+
+            
+            calculate.PlotSignalDataCal("Bitcoin", cryptoData, openDate, endDate);
+
+            
+            var filteredData = cryptoData.Where(c => c.Date >= openDate && c.Date <= endDate).ToList();
+            Assert.AreEqual(2, filteredData.Count);
+            Assert.AreEqual(new DateTime(2021, 1, 2), filteredData[0].Date);
+            Assert.AreEqual(new DateTime(2021, 1, 3), filteredData[1].Date);
         }
 
+        /// <summary>
+        /// on appel la vrais methode search avec des donnée fictive de début et de fin pour voir si cela fonctionne
+        /// </summary>
         [TestMethod()]
         public void searchTest()
         {
-            Assert.Fail();
+            var calculate = new Calculate();
+            DateTime debut = new DateTime(2021, 1, 1);
+            DateTime fin = new DateTime(2021, 1, 3);
+
+            // donnée fictive
+            var bitcoinData = new List<CryptoData>
+            {
+                new CryptoData { Date = new DateTime(2021, 1, 1), Close = 30000 },
+                new CryptoData { Date = new DateTime(2021, 1, 2), Close = 32000 },
+                new CryptoData { Date = new DateTime(2021, 1, 3), Close = 31000 },
+            };
+
+            
+            calculate.search(debut, fin);
+
+
+            Assert.IsNotNull(bitcoinData);
+            Assert.AreEqual(3, bitcoinData.Count);
         }
+
     }
 }
